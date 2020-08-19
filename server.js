af732dd6af6f483a76e41792bfc409bec7898679
Samuel <ssscherisme@gmail.com>
2020-08-19 11:15:28 -0400

    tester code

---
+++ server.js
@@ -0,0 +1,43 @@
+// dependencies
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+//Connection 
+
+const PORT = process.env.PORT || 3000;
+
+// middleware
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
+const mongoURI = process.env.MONGO_URI;
+
+
+// mongoose connection
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log("connected to mongo");
+})
+
+// Controller
+
+const fruitsController = require('./controllers/fruits.js');clear
+app.use('/fruits', fruitsController);
+
+
+const vegetablesController = require("./controllers/vegetables.js");
+app.use("/vegetables", vegetablesController);
+
+
+// listen
+
+app.listen(PORT, () => {
+    console.log('listening on: ' + PORT);
+});
